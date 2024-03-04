21bea5ac6d60d08472bd3820574a4130c974f0cf
Sol <112528499+ItBeSollo@users.noreply.github.com>
2024-03-04 19:31:26 +0000

    Update main.py
    Discord.py rewrite

--- main.py
+++ main.py
@@ -1,31 +1,23 @@
-import disnake
-import random
-import asyncio
-import json
-from disnake.ext import commands
+import discord
+from discord.ext import commands
 
-# Define the intents for your bot
-intents = disnake.Intents.default()
+intents = discord.Intents.default()
 intents.members = True
-intents.message_content = True  # Enable the message content intent
-
-# Create a Disnake bot instance with the defined intents
+intents.message_content = True
 bot = commands.Bot(command_prefix=';', intents=intents)
 
 cogs = ['trivia', 'users', 'safari', 'battle', 'raids', 'pokemon', 'inventory', 'admin']
-
-for cog in cogs:
-    try:
-        bot.load_extension(cog)
-        print(f'Cog "{cog}" loaded successfully.')
-    except Exception as e:
-        print(f'Failed to load cog "{cog}": {e}')
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name} - {bot.user.id}')
-    print('Bot is ready to start processing events!')
-    game = disnake.Game("In Development")
+    print(f'Logged in as {bot.user}')
+    print(f'I am currently in {len(bot.guilds)} servers.')
+    game = discord.Game("In Development")
     await bot.change_presence(activity=game)
-  
-# Run the bot
-bot.run("token goes here")
+    for cog in cogs:
+        try:
+            await bot.load_extension(cog)
+            print(f'Cog "{cog}" loaded successfully.')
+        except Exception as e:
+            print(f'Failed to load cog "{cog}": {e}')
+
+bot.run("token")
